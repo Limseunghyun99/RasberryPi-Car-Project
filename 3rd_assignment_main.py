7becdba41a6358c6839a63d4fe57a7eedbd0367f
Seunghyun Lim <lcj3722@kookmin.ac.kr>
2018-11-16 14:23:05 +0900

    ...

--- 3rd_assignment_main.py
+++ 3rd_assignment_main.py
@@ -23,6 +23,43 @@ class myCar(object):
     # Complete the code to perform Third Assignment
     # =======================================================================
     def car_startup(self):
+        # implement the assignment code here
+        distance = 0
+        count = 0
+
+        self.car.accelerator.go_forward(50)
+        while self.car.line_detector.read_digital() != [1,1,1,1,1]:
+            if self.car.line_detector.is_center() == True:
+                if self.car.line_detector.read_digital()[1] == 1:
+                    self.car.steering.turn_left(10)
+                if self.car.line_detector.read_digital()[3] == 1:
+                    self.car.steering.turn_right(10)
+                else:
+                    self.car.steering.center_alignment()
+            elif self.car.line_detector.read_digital()[0] == 1:
+                self.car.accelerator.leftLarge()
+                if self.car.line_detector.is_center == True:
+                    self.car.steering.center_alignment()
+            elif self.car.line_detector.read_digital()[4] == 1:
+                self.car.steering.turn_right(30)
+            elif self.car.line_detector.read_digital() == [0,0,0,0,0]:
+                self.car.accelerator.go_backward(50)
+
+            for i in range(0,3):
+                count += self.car.distance_detector.get_distance()
+            distance = count/3
+            count = 0
+
+            if distance < 30:
+                self.car.steering.turn_left(30)
+                if self.car.line_detector.is_in_line():
+                    self.car.steering.turn_right(30)
+
+
+            if self.car.distance_detector.get_distance()
+            if self.car.line_detector.read_digital() == [1,1,1,1,1]:
+                self.car.accelerator.stop()
+
         pass
 
 
