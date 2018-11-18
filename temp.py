5bf15c0282cfbf8be657507f8d06366740da028d
Limseunghyun99 <lcj3722@kookmin.ac.kr>
2018-11-18 19:34:18 +0900

    test for indent

--- temp.py
+++ temp.py
@@ -24,78 +24,101 @@ class myCar(object):
     # =======================================================================
     def car_startup(self):
         # implement the assignment code here
+        self.car.steering.turning_max = 50
         distance = self.car.distance_detector.get_distance()
-        self.car.accelerator.go_forward(40)
-        count = 0
+        self.car.accelerator.go_forward(80)
         print("start")
-        countstop = 0
         while(True):
-            print(self.car.line_detector.read_digital())
+            print("tracking: ", self.car.line_detector.read_digital())
             distance = self.car.distance_detector.get_distance()
-            print(distance)
-            if distance == -1:
-                continue
-            if distance <= 20:
-                count += 1
-            if count >= 5:
+            print("distance: ", distance)
+            if 0 < distance <= 30:
                 self.car.accelerator.stop()
-                self.car.steering.turn_left(55)
-                self.car.accelerator.go_forward(50)
-                time.sleep(1)
-                self.car.accelerator.stop()
-                self.car.steering.turn_right(165)
-                self.car.accelerator.go_forward(80)
-                time.sleep(1.9)
-                countstop += 1
-                print(countstop)
-                count = 0
-                while(True):
-                    if(self.car.line_detector.read_digital() != [0,0,0,0,0]):
-                        break
+                self.car.steering.turn(43)
+                self.car.accelerator.go_forward(40)
+                time.sleep(0.5)
+                    while self.car.line_detector.read_digital() != [1,0,0,0,0]:
+                        self.car.accelerator.go_forward(60)
+                    self.car.steering.turn(120)
+                    self.car.accelerator.go_forward(50)
+                    time.sleep(1.5)
+                    while self.car.line_detector.read_digital()[4] == 0:
+                        self.car.accelerator.go_forward(60)
+                    
                 
             if(self.car.line_detector.read_digital() == [0,0,1,0,0]):
                 self.car.accelerator.go_forward(40)
-            elif(self.car.line_detector.read_digital() == [1,0,0,0,0]):
+
+            elif(self.car.line_detector.read_digital() == [1,1,1,1,0]):
+                self.car.steering.turn_left(50)
+                self.car.accelerator.go_forward(50)
+
+            elif(self.car.line_detector.read_digital() == [1,0,0,0,0] ):
                 self.car.steering.turn_left(60)
                 self.car.accelerator.go_forward(40)
+
             elif(self.car.line_detector.read_digital() == [1,1,0,0,0]):
                 self.car.steering.turn_left(65)
                 self.car.accelerator.go_forward(40)
+
             elif(self.car.line_detector.read_digital() == [0,1,0,0,0]):
-                self.car.steering.turn_left(80)
+                self.car.steering.turn_left(70)
                 self.car.accelerator.go_forward(40)
+
             elif(self.car.line_detector.read_digital() == [0,1,1,0,0]):
-                self.car.steering.turn_left(85)
+                self.car.steering.turn_left(70)
                 self.car.accelerator.go_forward(40)
+
             elif(self.car.line_detector.read_digital() == [1,1,1,0,0]):
-                self.car.steering.turn_left(87)
+                self.car.steering.turn_left(75)
                 self.car.accelerator.go_forward(40)
+
             elif(self.car.line_detector.read_digital() == [0,0,1,1,0]):
                 self.car.steering.turn_right(95)
                 self.car.accelerator.go_forward(40)
+
             elif(self.car.line_detector.read_digital() == [0,0,1,1,1]):
-                self.car.steering.turn_right(100)
+                self.car.steering.turn_right(95)
                 self.car.accelerator.go_forward(40)
+
             elif(self.car.line_detector.read_digital() == [0,0,0,1,0]):
                 self.car.steering.turn_right(110)
                 self.car.accelerator.go_forward(40)
+
             elif(self.car.line_detector.read_digital() == [0,0,0,1,1]):
-                self.car.steering.turn_right(115)
+                self.car.steering.turn_right(105)
                 self.car.accelerator.go_forward(40)
+
             elif(self.car.line_detector.read_digital() == [0,0,0,0,1]):
-                self.car.steering.turn_right(120)
+                self.car.steering.turn_right(110)
                 self.car.accelerator.go_forward(40)
+                time.sleep(0.1)
+
+            elif(self.car.line_detector.read_digital() == [0,1,1,1,0]):
+                self.car.steering.turn(90)
+                self.car.accelerator.go_forward(40)
+
+            elif(self.car.line_detector.read_digital() == [1,0,0,1,1]):
+                self.car.steering.turn_right(110)
+                self.car.accelerator.go_forward(60)
+
+            elif(self.car.line_detector.read_digital() == [1,0,1,1,1]):
+                self.car.steering.turn_right(110)
+                self.car.accelerator.go_forward(50)
+
             elif(self.car.line_detector.read_digital() == [0,0,0,0,0]):
-                self.car.steering.turn(100)
+                self.car.steering.turn_right(120)
                 self.car.accelerator.go_backward(40)
                 time.sleep(0.33)
+                self.car.steering.turn(80)
                 self.car.accelerator.stop()
+
             elif(self.car.line_detector.read_digital() == [1,1,1,1,1]):
-                if countstop == 4:
-                    self.car.accelerator.stop()
-                    break
+                self.car.accelerator.stop()
+                break
             else:
                 continue
+            time.sleep(0.1)
         #self.car.accelerator.stop()
                 
 if __name__ == "__main__":
@@ -106,4 +129,4 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         # when the Ctrl+C key has been pressed,
         # the moving object will be stopped
-myCar.drive_parking() 
+        myCar.drive_parking() 
